6aa2ee3c183f622c5d0391c1fed29c8fa13287e9
alaykakeyt <alaicakyte@gmail.com>
2025-02-23 18:07:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function addNumbers(a, b) {
+    return a + b;
+}
+
+var a = Number(prompt("Enter the first number:"));
+var b = Number(prompt("Enter the second number:"));
+
+alert("The sum is: " + (a, b));
