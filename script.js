a2a7808516d7c1b49b070ed554eb9b167d36eda0
alaykakeyt <alaicakyte@gmail.com>
2025-02-23 18:26:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-function addNumbers(a, b) {
-    return a + b;
+function addnumbers() {
+   var a = Number(prompt("Enter the first number: "));
+   var b = Number(prompt("Enter theh second Number: "));
+   alert("The sum is " + (a + b));
 }
-
-var a = Number(prompt("Enter the first number:"));
-var b = Number(prompt("Enter the second number:"));
-
-alert("The sum is: " + addNumbers(a + b));
+addnumbers();
